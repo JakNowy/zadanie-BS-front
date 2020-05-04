2d048537d038ec4cba1f9a9a4d842148d510b3b6
Kobas <kobas394@gmail.com>
2020-05-04 14:27:30 +0200

    .

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,139 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="flex flex-center q-ma-lg">
+    <q-card class="q-pa-lg">
+      <q-dialog v-model="detailMenuDialog" full-height full-width>
+        <MenuDetails
+        :response="detailResponse"
+        />
+      </q-dialog>
+
+      <q-table
+        style="min-height: 350px"
+        :columns="tableColumns"
+        :data="tableData"
+        @row-click="handleRowClick"
+        row-key="name"
+      >
+        <template v-slot:bottom>
+          <div style="width: 100%" class="row q-pt-md">
+
+            <div class="col-md-4"></div>
+
+            <div class="col-md-4 justify-around flex">
+              <q-btn @click="fetchMenus(pagination.prev, null, null)" :disable="!pagination.prev">
+                Wstecz
+              </q-btn>
+
+              <q-btn @click="fetchMenus(pagination.next, null, null)" :disable="!pagination.next">
+                Dalej
+              </q-btn>
+            </div>
+
+            <div class="col-md-4 justify-end flex">
+              <q-select
+                style="width: 80%"
+                filled
+                dense
+                options-dense
+                v-model="orderring"
+                label="Sortuj"
+                :options="orderringOptions"
+                behavior="menu"
+              />
+            </div>
+
+          </div>
+        </template>
+
+        <template v-slot:top>
+          <div class="text-h5">Menu dla Ciebie:</div>
+        </template>
+      </q-table>
+    </q-card>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios';
+import MenuDetails from "@/components/MenuDetails";
+import dayjs from "dayjs";
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    MenuDetails
+  },
+  mounted() {
+    this.fetchMenus('http://127.0.0.1:8000/api/menus/?', null, 3, null)
+  },
+  data () {
+    return {
+      detailResponse: null,
+      detailMenuDialog: false,
+      pagination: {
+        offset: 0,
+        rowsPerPage: 3, //limit
+        count: null
+      },
+      orderringOptions: [
+        {label: 'Nazwa', value: 'name'},
+        {label: 'Nazwa (malejaco)', value: '-name'},
+        {label: 'Liczba dan', value: 'dish_count'},
+        {label: 'Liczba dan (malejaco)', value: '-dish_count'},
+      ],
+      orderring: {
+        label: 'Nazwa',
+        value: 'id',
+      },
+      tableColumns: [
+        { name: 'id', label: 'ID', field: 'id', sortable: false , align: 'left' },
+        { name: 'Restauracja', label: 'Restauracja', field: 'company_name', sortable: true, align: 'center'},
+        { name: 'Nazwa', label: 'Nazwa', field: 'name', sortable: true , align: 'center' },
+        { name: 'Opis', label: 'Opis', field: 'description', align: 'center' },
+        { name: 'Dania', label: 'Liczba dań', field: 'dish_count', sortable: true , align: 'center' },
+        { name: 'Modyfikacja', label: 'Data modyfikacji', field: 'date_modified', sortable: false , align: 'center',
+          format: val => dayjs(val).format('DD-MM-YYYY') },
+        { name: 'Dodano', label: 'Data dodania', field: 'date_created', sortable: false , align: 'center',
+          format: val => dayjs(val).format('DD-MM-YYYY')},
+      ],
+      tableData: [
+      ],
+    }
+  },
+  filters: {
+    formatDate: function (isoDate) {
+      return isoDate ? dayjs(isoDate).format('ddd MMM YYY') : ''
+    }
+  },
+  watch: {
+    orderring: function () {
+      this.fetchMenus('http://127.0.0.1:8000/api/menus/?', null, this.pagination.rowsPerPage, this.orderring.value)
+    }
+  },
+  methods: {
+    fetchMenus (url, offset, limit, orderring) {
+      offset = offset ? `&offset=${offset}` : ''
+      limit = limit ? `&limit=${limit}` : ''
+      orderring = orderring ? `&orderring=${orderring}` : ''
+      url = url + orderring + limit + offset
+      axios.get(url).then((response) => {
+        this.pagination.rowsNumber = response.data.count
+        this.pagination.count = response.data.count
+        this.pagination.next = response.data.next
+        this.pagination.prev = response.data.previous
+        this.tableData = response.data.results
+      }).catch((error) => {
+        console.log(error)
+      })
+    },
+    handleRowClick (event, row) {
+      this.detailMenuDialog = true
+      let url = 'http://127.0.0.1:8000/api/menus/' + row.id
+      axios.get(url).then((response) => {
+        this.detailResponse = response
+      })
+    },
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/MenuDetails.vue
@@ -0,0 +1,47 @@
+<template>
+<div style="margin: 0 20% 0 20%">
+    <q-card class="fit q-pa-xl">
+        <q-table
+        :title="`${response.data.company_name}: ${response.data.name}`"
+        class="q-mx-lg"
+        dense
+        :data="response.data.dishes"
+        :columns="detailColumns"
+        >
+            <template v-slot:top-right>
+
+            </template>
+            <template v-slot:bottom></template>
+
+        </q-table>
+        <p>
+            Opis restauracji: {{ response.data.description }}
+        </p>
+    </q-card>
+</div>
+</template>
+
+<script>
+export default {
+    name: "MenuDetails",
+    props: {
+        response : Object,
+    },
+    mounted () {
+        console.log(this.props.response)
+    },
+    data () {
+        return {
+            detailColumns: [
+                { name: 'name', label: 'Nazwa', field: 'name', sortable: false , align: 'left' },
+                { name: 'description', label: 'Opis', field: 'description', sortable: false , align: 'center' },
+                { name: 'is_vegan', label: 'Vegan', field: 'is_vegan', sortable: false , align: 'center', format: val => val ? 'Tak' : 'Nie'},
+                { name: 'price', label: 'Cena', field: 'price', sortable: false , align: 'center', format: val => `${val} zl`},
+            ],
+        }
+    },
+}
+</script>
+
+<style scoped>
+</style>
